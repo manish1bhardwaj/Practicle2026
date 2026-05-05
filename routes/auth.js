abf87179319ba8383b7166b08d3ec38cf6beae1b
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2026-05-05 12:07:27 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,16 +5,8 @@ router.get('/login', (req, res) => {
   res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
-  res.redirect('/');
-});
-
 router.get('/register', (req, res) => {
   res.render('auth/register');
-});
-
-router.post('/register', (req, res) => {
-  res.redirect('/');
 });
 
 router.get('/logout', (req, res) => {
