ca170c70038d16e773aaae2c0955e88e35727b2c
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2026-05-05 12:09:05 +0530

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('movies/index', { movies: [], user: null });
+  res.render('movies/index');
 });
 
 router.get('/new', (req, res) => {
